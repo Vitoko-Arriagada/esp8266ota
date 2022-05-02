6a737f0df2e665a92f7e329fff6dc6d95dc3bfe4
Victor Arriagada Martinez <veamchile@gmail.com>
2022-05-01 20:14:11 -0400

    Domingo 1 de mayo 2022

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <LittleFS.h>
+#include <FS.h>
 #include <MQTT.h>
 #include <EEPROM.h>
 #include <ESP_OTA_GitHub.h>
@@ -21,7 +21,7 @@ BearSSL::CertStore certStore;
 /* Set up values for your repository and binary names */
 #define GHOTA_USER "Vitoko-Arriagada"
 #define GHOTA_REPO "esp8266ota"
-#define GHOTA_CURRENT_TAG "0.0.6"
+#define GHOTA_CURRENT_TAG "0.0.16"
 #define GHOTA_BIN_FILE "firmware.bin"
 #define GHOTA_ACCEPT_PRERELEASE 0
 
@@ -39,6 +39,8 @@ unsigned char mac[6];
 
 unsigned long lastMillis;
 String clientMac = "";
+unsigned long contador = 0;
+bool update = false;
 
 WiFiClientSecure net;
 MQTTClient mqttclient(512);
@@ -48,6 +50,7 @@ struct MyObject
   int EEPROM_OK;
   int FREC_ENVIO;
   int INICIOS;
+  bool UPDATE;
 };
 
 unsigned long DELAY = 10000;
@@ -65,11 +68,13 @@ void grabar_parametros()
   MyObject parametro = leer_parametros();
   DELAY = parametro.FREC_ENVIO * 1000;
   REINICIOS = parametro.INICIOS;
+  update = parametro.UPDATE;
   if (parametro.EEPROM_OK != 2022)
   {
     parametro.EEPROM_OK = 2022;
     parametro.FREC_ENVIO = 1;
     parametro.INICIOS = 0;
+    parametro.UPDATE = false;
     EEPROM.put(0, parametro);
     EEPROM.commit();
   }
@@ -82,19 +87,7 @@ void grabar_parametros()
 }
 
 void handle_upgade()
-{
-
-  LittleFS.begin();
-  int numCerts = certStore.initCertStore(LittleFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
-  Serial.print(F("Number of CA certs read: "));
-  Serial.println(numCerts);
-  if (numCerts == 0)
-  {
-    Serial.println(F("No certs found. Did you run certs-from-mozill.py and upload the SPIFFS directory before running?"));
-    return; // Can't connect to anything w/o certs!
-  }
-
-  // Initialise Update Code
+{ // Initialise Update Code
   // We do this locally so that the memory used is freed when the function exists.
   ESPOTAGitHub ESPOTAGitHub(&certStore, GHOTA_USER, GHOTA_REPO, GHOTA_CURRENT_TAG, GHOTA_BIN_FILE, GHOTA_ACCEPT_PRERELEASE);
 
@@ -122,24 +115,25 @@ void handle_upgade()
 
 void enviarInfo()
 {
-  DynamicJsonDocument doc(1024);
+  // DynamicJsonDocument doc(1024);
 
-  JsonObject doc_0 = doc.createNestedObject("Info");
-  doc_0["IP"] = WiFi.localIP();
-  doc_0["SSID"] = WiFi.SSID();
-  doc_0["Version Firmware"] = GHOTA_CURRENT_TAG;
-  doc_0["Fabricante"] = fabricante;
-  doc_0["Nombre Dispositivo"] = nombre;
-  doc_0["Modelo Dispositivo"] = modelo;
-  doc_0["Config"]["Periodo de envio"] = 10;
-  doc_0["Config"]["Periodo de puerta abierta"] = 20;
-  doc_0["Config"]["Modo"] = 30;
-  String output;
+  // JsonObject doc_0 = doc.createNestedObject("info");
+  // doc_0["IP"] = WiFi.localIP();
+  // doc_0["SSID"] = WiFi.SSID();
+  // doc_0["Version Firmware"] = GHOTA_CURRENT_TAG;
+  // doc_0["Fabricante"] = fabricante;
+  // doc_0["Nombre Dispositivo"] = nombre;
+  // doc_0["Modelo Dispositivo"] = modelo;
+  // doc_0["Config"]["Periodo de envio"] = DELAY / 1000;
+  // doc_0["Config"]["Periodo de puerta abierta"] = 20;
+  // doc_0["Config"]["Modo"] = 30;
+  // String output;
 
-  Serial.println(serializeJson(doc, output));
+  // Serial.println(serializeJson(doc, output));
   Serial.println("\nconnected!");
   Serial.println("\nSuscrito: " + topicoBase + "/comandos");
-  mqttclient.publish(topicoBase + "/info", output);
+  // mqttclient.publish(topicoBase + "/info", output);
+  mqttclient.publish(topicoBase + "/status", "{\"Status\":\"ON LINE\"}");
 }
 
 void connect()
@@ -155,7 +149,7 @@ void connect()
   // check the following example for methods to verify the server:
   // https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/examples/BearSSL_Validation/BearSSL_Validation.ino
   net.setInsecure();
-
+  mqttclient.setWill((topicoBase + String("/status")).c_str(), "{\"Status\":\"OFF LINE\"}");
   Serial.print("\nconnecting...");
   while (!mqttclient.connect(clientid, username, token))
   {
@@ -190,7 +184,6 @@ void messageReceived(String &topic, String &payload)
       if (String(comando) == "SetSendFrecuency")
       {
         int valor = doc["valor"];
-        Serial.println("          COMANDO VALIDO");
         parametro.FREC_ENVIO = valor;
         DELAY = parametro.FREC_ENVIO * 1000;
         Serial.println(DELAY);
@@ -205,13 +198,30 @@ void messageReceived(String &topic, String &payload)
           ESP.restart();
         }
       }
+      else if (String(comando) == "Led")
+      {
+        bool valor = doc["valor"];
+        if (valor)
+        {
+          digitalWrite(2, LOW);
+        }
+        else
+        {
+          digitalWrite(2, HIGH);
+        }
+      }
     }
     else
     {
       if (payload != GHOTA_CURRENT_TAG)
       {
-        Serial.println("--------------------------------------");
-        buscar_nuevo_firmware();
+        Serial.println("Buscando Actualizacion...");
+        MyObject parametro = leer_parametros();
+        parametro.UPDATE = true;
+        EEPROM.put(0, parametro);
+        EEPROM.commit();
+        delay(2000);
+        ESP.restart();
       }
       else
       {
@@ -244,7 +254,7 @@ void setup()
   // Start serial for debugging (not used by library, just this sketch).
   pinMode(2, OUTPUT);
   pinMode(0, INPUT);
-
+  digitalWrite(2, HIGH);
   // Connect to WiFi
   Serial.print("Connecting to WiFi... ");
   WiFi.mode(WIFI_STA);
@@ -258,6 +268,25 @@ void setup()
   clientMac += macToStr(mac);
   Serial.println("MAC: " + clientMac);
   topicoBase = String(fabricante) + "/" + String(proyecto) + "/ID" + String(ESP.getChipId(), 10);
+
+  SPIFFS.begin();
+  int numCerts = certStore.initCertStore(SPIFFS, PSTR("/certs.idx"), PSTR("/certs.ar"));
+  Serial.print(F("Number of CA certs read: "));
+  Serial.println(numCerts);
+  if (numCerts == 0)
+  {
+    Serial.println(F("No certs found. Did you run certs-from-mozill.py and upload the SPIFFS directory before running?"));
+    return; // Can't connect to anything w/o certs!
+  }
+  if (update)
+  {
+    MyObject parametro = leer_parametros();
+    parametro.UPDATE = false;
+    EEPROM.put(0, parametro);
+    EEPROM.commit();
+    buscar_nuevo_firmware();
+  }
+
   /* This is the actual code to check and upgrade */
   // handle_upgade();
   mqttclient.begin("iot.veamchile.cl", 8883, net);
@@ -279,10 +308,13 @@ void loop()
 
   if (digitalRead(0) == 0)
   {
-    ESP.restart();
     Serial.println("Buscando Actualizacion...");
-    buscar_nuevo_firmware();
-    handle_upgade();
+    MyObject parametro = leer_parametros();
+    parametro.UPDATE = true;
+    EEPROM.put(0, parametro);
+    EEPROM.commit();
+    delay(2000);
+    ESP.restart();
   }
 
   if (millis() - lastMillis > DELAY)
@@ -290,21 +322,37 @@ void loop()
     lastMillis = millis();
 
     DynamicJsonDocument doc(1024);
-    JsonObject doc_0 = doc.createNestedObject("data");
-    doc_0["Temp"] = float(random(1, 10000)) / 100;
-    doc_0["Hum"] = float(random(1, 10000)) / 100;
-    doc_0["Volt Batt"] = float(random(0, 500)) / 100;
-    doc_0["Cargando"] = random(0, 2);
-    doc_0["Puerta"] = random(0, 2);
-    doc_0["Boton"] = random(0, 2);
-    doc_0["Memoria RAM"] = ESP.getFreeHeap();
-    doc_0["Motivo Reinicio"] = ESP.getResetReason();
-    doc_0["Reinicios"] = REINICIOS;
-    doc_0["RSSI"] = WiFi.RSSI();
+    JsonObject device = doc.createNestedObject("device");
+    device["ip"] = WiFi.localIP();
+    ;
+    device["ssid"] = WiFi.SSID();
+    device["versionFirmware"] = GHOTA_CURRENT_TAG;
+    device["fabricante"] = fabricante;
+    device["nombreDispositivo"] = nombre;
+    device["modeloDispositivo"] = modelo;
+
+    JsonObject device_config = device.createNestedObject("config");
+    device_config["periodoEnvio"] = DELAY / 1000;
+    device_config["periodoPuertaAbierta"] = 20;
+    device_config["modo"] = 30;
+
+    JsonObject sensors = doc.createNestedObject("sensors");
+    sensors["temp"] = float(random(1, 10000)) / 100;
+    sensors["hum"] = float(random(1, 10000)) / 100;
+    sensors["voltBatt"] = float(random(0, 500)) / 100;
+    sensors["cargando"] = random(0, 2) ? true : false;
+    sensors["puerta"] =  random(0, 2) ? true : false;;
+    sensors["boton"] =  random(0, 2) ? true : false;;
+    sensors["memoriaRAM"] = ESP.getFreeHeap();
+
+    JsonObject sensors_Reinicios = sensors.createNestedObject("reinicios");
+    sensors_Reinicios["motivo"] = ESP.getResetReason();
+    sensors_Reinicios["contador"] = REINICIOS;
+    sensors["contadorDatos"] = contador++;
+    sensors["rssi"] = WiFi.RSSI();
 
     String output;
     Serial.println(serializeJson(doc, output));
     mqttclient.publish(topicoBase + "/data", output);
-    texto = texto + "123456789012345678901234567890123456789012345678901234567890";
   }
 }
